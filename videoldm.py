03d83b95b6dd2abd642cefee614cd0435a6dfd9a
srpkdyy <srpkdyy@gmail.com>
2023-05-19 01:18:25 +0900

    implementd temporal layers

--- videoldm.py
+++ videoldm.py
@@ -1,24 +1,49 @@
 import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
 from einops.layers.torch import Rearrange
-from diffusers import UNet2DConditionModel
 
 
-class ResBlock3D(nn.Module):
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim, max_pos=512):
+        super().__init__()
+
+        pos = torch.arange(max_pos)
+
+        freq = torch.arange(dim//2) / dim
+        freq = (freq * torch.tensor(10000).log()).exp()
+
+        x = rearrange(pos, 'L -> L 1') / freq
+        x = rearrange(x, 'L d -> L d 1')
+
+        pe = torch.cat((x.sin(), x.cos()), dim=-1)
+        self.pe = rearrange(pe, 'L d sc -> L (d sc)')
+
+        self.dummy = nn.Parameter(torch.rand(1))
+
+    def forward(self, length):
+        enc = self.pe[:length]
+        enc = enc.to(self.dummy.device)
+        return enc
+
+
+class Conv3DLayer(nn.Module):
     def __init__(self, in_dim, out_dim, n_frames):
         super().__init__()
 
         self.to_3d = Rearrange('(b t) c h w -> b c t h w', t=n_frames)
         self.to_2d = Rearrange('b c t h w -> (b t) c h w')
 
+        k, p = (3, 1, 1), (1, 0, 0)
         self.block1 = nn.Sequential(
             nn.GroupNorm(32, in_dim),
             nn.SiLU(),
-            nn.Conv3d(in_dim, out_dim, kernel_size=3, stride=1, padding=1)
+            nn.Conv3d(in_dim, out_dim, kernel_size=k, stride=1, padding=p)
         )
         self.block2 = nn.Sequential(
             nn.GroupNorm(32, out_dim),
             nn.SiLU(),
-            nn.Conv3d(out_dim, out_dim, kernel_size=3, stride=1, padding=1)
+            nn.Conv3d(out_dim, out_dim, kernel_size=k, stride=1, padding=p)
         )
 
         self.alpha = nn.Parameter(torch.ones(1))
@@ -38,7 +63,42 @@ class ResBlock3D(nn.Module):
         return out
 
 
+class TemporalAttentionLayer(nn.Module):
+    def __init__(self, dim, n_frames, n_heads=8):
+        super().__init__()
+        self.n_frames = n_frames
+
+        self.pos_enc = PositionalEncoding(dim)
+
+        kv_dim = dim // n_heads
+        proj_dim = kv_dim * n_heads
+        self.q_proj = nn.Linear(dim, proj_dim, bias=False)
+        self.k_proj = nn.Linear(dim, proj_dim, bias=False)
+        self.v_proj = nn.Linear(dim, proj_dim, bias=False)
+        self.o_proj = nn.Linear(proj_dim, dim, bias=False)
+
+        self.alpha = nn.Parameter(torch.ones(1))
+
+    def forward(self, q, kv=None, mask=None):
+        kv = kv if kv is not None else q
+
+        bt, c, h, w = q.shape
+        q = rearrange(q, '(b t) c h w -> (b h w) t c', t=self.n_frames)
+
+        q = q + self.pos_enc(self.n_frames)
+
+        qkv = torch.stack([self.q_proj(q), self.k_proj(kv), self.v_proj(kv)])
+        q, k, v = rearrange(qkv, 'qkv bhw t (h d) -> qkv bhw h t d', h=self.n_heads)
+
+        out = F.scaled_dot_product_attention(q, k, v, mask)
+        out = rearrange(out, 'bhw h t d -> bhw t (h d)')
+        out = self.o_proj(out)
+
+        out = rearrange('(b h w) t c -> (b t) c h w', h=h, w=w)
+        return out
+
+
 class VideoLDM(UNet2DConditionModel):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        pass
 
